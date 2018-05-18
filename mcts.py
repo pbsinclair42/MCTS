ed5d65c21b96c33c44113d550751749a7e273b59
pbsinclair42 <bobsmith0030+git2@gmail.com>
2018-05-18 13:24:54 +0100

    Move tree node to main mcts file

--- mcts.py
+++ mcts.py
@@ -1,7 +1,6 @@
 import time
 import math
 import random
-from node import treeNode
 
 
 def randomPolicy(state):
@@ -12,6 +11,20 @@ def randomPolicy(state):
             raise Exception("Non-terminal state has no possible actions: " + str(state))
         state = state.takeAction(action)
     return state.getReward()
+
+
+class treeNode():
+    def __init__(self, state, parent):
+        self.state = state
+        self.isTerminal = state.isTerminal()
+        self.isFullyExpanded = self.isTerminal
+        self.parent = parent
+        self.numVisits = 0
+        self.totalReward = 0
+        self.children = {}
+
+    def __eq__(self, other):
+        return self.__class__ == other.__class__ and self.state == other.state
 
 
 class mcts():
