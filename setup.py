bba06d4594d891507b89744a664dd518c54d3c48
pbsinclair42 <bobsmith0030+git2@gmail.com>
2018-12-17 22:10:18 +0000

    Update setup.py
    Fix publishing single file rather than package and add classifiers

--- setup.py
+++ setup.py
@@ -2,15 +2,29 @@ from setuptools import setup
 
 setup(
     name='mcts',
-    packages=['mcts'],
-    version='1.0.1',
+    py_modules=['mcts'],
+    version='1.0.3',
     description='A simple package to allow users to run Monte Carlo Tree Search on any perfect information domain',
     long_description=open("README.md", "r").read(),
     long_description_content_type='text/markdown',
     author='Paul Sinclair',
     author_email='pbsinclair42@gmail.com',
+    license='MIT',
     url='https://github.com/pbsinclair42/MCTS',
-    download_url='https://github.com/pbsinclair42/MCTS/archive/1.0.tar.gz',
     keywords=['mcts', 'monte', 'carlo', 'tree', 'search'],
-    classifiers=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
 )
